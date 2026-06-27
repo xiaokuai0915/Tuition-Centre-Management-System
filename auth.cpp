85d25051a9f63f43c103717c660c27fd090eba61
xiaokuai <xiaokuai0915@users.noreply.github.com>
2026-06-27 13:57:09 +0800

    Update 1.04

--- auth.cpp
+++ auth.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include "auth.h"
+#include "models.h"
 #include <string>
 #include <iostream>
 #include <limits>
@@ -8,13 +9,13 @@
 void registerUser() {
     User newUser;
     bool registration = false;
-    
+
     std::cout << "Create username: "; std::cin >> newUser.username;
     std::cout << "Create password: "; std::cin >> newUser.password;
 
     do {
         std::cout << "Enter level (Admin=0, User=1):";
-        if (!(std::cin >> newUser.level)) {
+        if (!(std::cin >> newUser.level)) { //filter input
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
@@ -22,12 +23,12 @@ void registerUser() {
 
     if (newUser.level == 0) {
         std::cout << "Enter Admin code: ";
-        if (!(std::cin >> newUser.code)) {
+        if (!(std::cin >> newUser.code)) { //filter input
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
 
-        if (newUser.code == newUser.adminCode) {
+        if (newUser.code == newUser.adminCode) { //detect admind code from menu.h
             registration = true;
         }
         else {
@@ -37,11 +38,12 @@ void registerUser() {
     else {
         registration = true;
     }
-    
+
     if (registration == true) {
-        //ofstream is create nor edit the file, ios::app is to keep the previous thing that alr available inside the file so it wont get wipe out when running this row
+        //ofstream is create or edit the file, ios::app is to keep the previous thing that alr available inside the file so it wont get wipe out when running this row
         std::ofstream outFile("user.txt", std::ios::app);
-        outFile << newUser.username << " " << newUser.password << " " << newUser.level << "\n";
+        outFile << newUser.username << " " << newUser.password << " " << newUser.level << "\n"; // store username , password, level one by one
+        outFile.close();//close the file to avoid error input into the file
         std::cout << "Registered successfully! Please log in now!\n";
     }
     else {
@@ -50,19 +52,25 @@ void registerUser() {
 }
 
 //Check name and password from txt file and send result to main
-bool login() {
-	std::string inputU, inputP;
-	std::cout << "Username: "; std::cin >> inputU;
-	std::cout << "Password: "; std::cin >> inputP;
+bool login(User& currentUser) {
+    std::string inputU, inputP;
+    std::cout << "Username: "; std::cin >> inputU;
+    std::cout << "Password: "; std::cin >> inputP;
 
-	std::ifstream inFile("user.txt"); //ifstream means read the file
-	std::string fileU, fileP;
+    std::ifstream inFile("user.txt"); //ifstream means read the file
+    std::string fileU, fileP;
     int fileL;
 
 
-	//i believe u understand by just watching here, it is just if input username = username in txt file then send true as output, if no then false, password either
-	while (inFile >> fileU >> fileP >> fileL) {
-		if (fileU == inputU && fileP == inputP) return true;
-	}
-	return false;
+   //if input username = username in txt file then send true as output, if no then false, password also
+    while (inFile >> fileU >> fileP >> fileL) {
+        std::cout << "[DEBUG] Comparing " << inputU << " with " << fileU << "\n"; //temporary for me to debug, i just leave it here until when we need to delete it :D
+
+        if (fileU == inputU && fileP == inputP) {
+            currentUser.username = inputU;
+            return true;
+        }
+        
+    }
+    return false;
 }
--- models.h
+++ models.h
@@ -2,19 +2,22 @@
 #define MODELS_H
 
 #include <string>
+#include <vector>
+//base struct for the system to create subject
+struct Course {
+	int id = 0;
+	std::string Name = "";
+	double price = 0.0;
+};
 
 //base struct for the system to create user
 struct User {
 	std::string username;
 	std::string password;
+	std::vector<Course> mypackage;
 	int level = 0;
-	double credit = 100.0;//100 credit for every new user
 	int code = 0;
 	int adminCode = 123456; //code to register as a admin
 };
 
-struct Food {
-	std::string name;
-	double price;
-};
 #endif
