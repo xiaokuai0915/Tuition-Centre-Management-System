d1eb1ff071dcd462875fba1a4405290881fc6e6b
xiaokuai <xiaokuai0915@users.noreply.github.com>
2026-06-27 13:54:45 +0800

    Update 1.04
    1.Change from food order to tuition centre
    2.Remove credit system
    3.Add view courses list
    4.Add system that you can take course into your package
    5.Add review your package course
    6.New storage system to store course taken by the user
    7.Markdown for everything for more understanding

--- models.h
+++ models.h
@@ -2,16 +2,22 @@
 #define MODELS_H
 
 #include <string>
+#include <vector>
+//base struct for the system to create subject
+struct Course {
+	int id = 0;
+	std::string Name = "";
+	double price = 0.0;
+};
 
 //base struct for the system to create user
 struct User {
 	std::string username;
 	std::string password;
-	double credit = 100.0;//100 credit for every new user
+	std::vector<Course> mypackage;
+	int level = 0;
+	int code = 0;
+	int adminCode = 123456; //code to register as a admin
 };
 
-struct Food{
-	std::string name;
-	double price;
-};
 #endif
