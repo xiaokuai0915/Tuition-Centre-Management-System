6b399c1513a0f5df0902133261a4489ef957bbb6
Mvict-66 <pianomusic0606@gmail.com>
2026-06-28 16:35:03 +0800

    Update models.h
    added role

--- models.h
+++ models.h
@@ -2,19 +2,25 @@
 #define MODELS_H
 
 #include <string>
+#include <vector>
+//base struct for the system to create subject
+struct Course {
+	int id = 0;
+	std::string Name = "";
+	double price = 0.0;
+};
 
 //base struct for the system to create user
 struct User {
 	std::string username;
 	std::string password;
+	std::vector<Course> mypackage;
+	int role = 0;
 	int level = 0;
-	double credit = 100.0;//100 credit for every new user
-	int code = 0;
+	int codeT = 0;
+	int teacherCode = 123; //code to register as a teacher
+	int codeA = 0;
 	int adminCode = 123456; //code to register as a admin
 };
 
-struct Food {
-	std::string name;
-	double price;
-};
 #endif
