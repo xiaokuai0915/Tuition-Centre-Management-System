037f02e6093668c55d84f2e619f66058ef48cad2
Kai Jun <ngkj-wm26@student.tarc.edu.my>
2026-06-28 16:40:33 +0800

    admin.cpp

--- admin.cpp
+++ admin.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include "adminauth.h"
-
+//add the below informs or else the program wrong run properly
 void openTuitionAdminDashboard() {
 	system("cls");
 	system("pause");
 }
-
+//I haven't add anything to the real main so I added int main here
 int main() {
 
 	int choice;
-
+//the dashboard for admin
 	while (true) {
 		system("cls");
 		std::cout << "===========================\n";
@@ -19,13 +19,13 @@ int main() {
 		std::cout << "2. Exit\n";
 		std::cout << "Enter input selection\n";
 		std::cin >> choice;
-
+//u can access the adminauth.h file if the password is correct
 		if (choice == 1) {
 			if (AdminAuth::authenticateAdmin()) {
 				openTuitionAdminDashboard();
 			}
 		}
-		else if (choice == 2) {
+		else if (choice == 2) { //password incorrect and return back to the dashboard
 			break;
 		}
 	}
