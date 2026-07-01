76f7756602948fcba1110e47f99a39010d71f50d
Mvict-66 <pianomusic0606@gmail.com>
2026-07-01 16:01:27 +0800

    Update auth.cpp
    added check name

--- auth.cpp
+++ auth.cpp
@@ -8,9 +8,29 @@
 //Register and save into txt file
 void registerUser() {
     User newUser;
+    bool checkname;
     bool registration = false;
 
-    std::cout << "Create username: "; std::cin >> newUser.username;
+
+    //check if username already exists
+    do {
+        checkname = false;
+        std::cout << "Create username: "; std::cin >> newUser.username;
+
+        std::ifstream inFile("user.txt"); //ifstream means read the file
+        std::string fileU, fileP;
+        int fileR;
+
+        while (inFile >> fileU >> fileP >> fileR) {
+            std::cout << "[DEBUG] Comparing " << newUser.username << " with " << fileU << "\n"; //same reason as login, for debugging purpose
+            if (newUser.username == fileU) {
+                std::cout << "Username already exist, please use another username.\n";
+                checkname = true;
+                break;
+            }
+        }
+    } while (checkname);
+
     std::cout << "Create password: "; std::cin >> newUser.password;
 
     do {
@@ -61,22 +81,14 @@ void registerUser() {
 
 //Check name and password from txt file and send result to main
 bool login(User& currentUser) {
-    currentUser.username = "";
-    currentUser.password = "";
-    currentUser.role = -1; //safe neutral state
-
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //clear out the traiing newline left over from the main menu
-
     std::string inputU, inputP;
-    std::cout << "Username: "; 
-    std::cin >> inputU;
-    std::cout << "Password: "; 
-    std::cin >> inputP;
+    std::cout << "Username: "; std::cin >> inputU;
+    std::cout << "Password: "; std::cin >> inputP;
 
     std::ifstream inFile("user.txt"); //ifstream means read the file
     std::string fileU, fileP;
     int fileR;
+
 
     //if input username = username in txt file then send true as output, if no then false, password also
     while (inFile >> fileU >> fileP >> fileR) {
@@ -84,13 +96,9 @@ bool login(User& currentUser) {
 
         if (fileU == inputU && fileP == inputP) {
             currentUser.username = inputU;
-            currentUser.password = inputP;
-            currentUser.role = fileR;
-            inFile.close();
             return true;
         }
 
     }
-    inFile.close();
     return false;
 }
