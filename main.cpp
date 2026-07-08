18b05c9259edfd561975d92af113300676ccae11
xiaokuai <83657259+xiaokuai0915@users.noreply.github.com>
2026-07-08 17:08:29 +0800

    Enhance input validation and error messages
    Added input validation for user choice and improved error handling.

--- main.cpp
+++ main.cpp
@@ -16,10 +16,20 @@ int main() {
 	while (running) {
 		std::cout << "\n--- Welcome System --\n";
 		std::cout << "1. Register\n2. Login\n3. Exit\nPlease choose one option by typing the number\n";
-
 		if (!(std::cin >> choice)) {//input filter
 			std::cin.clear();
 			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "Invalid input , please try again!";
+			continue; //continue means go back to the start of the loop
+		}
+		else{
+			char nextChar = std::cin.peek(); //peek to check the next character in the input buffer
+			if (nextChar != '\n' && nextChar !=' ' && nextChar !=EOF) { //if the next character is not a newline, it means there are extra characters in the input buffer
+				std::cin.clear(); //clear the error state of cin
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //ignore the rest of the line
+				std::cout << "Invalid input , please try again!";
+				continue; //go back to the start of the loop
+			}
 		}
 
 		//switch here get result from the choice and entering it to case for different result
@@ -28,19 +38,19 @@ int main() {
 			registerUser(); //take result from auth.cpp and continue
 			break; //break means end this case and go back to the choice section
 		case 2:
-	        if (login(currentUser)) { //call login exactly once
-	            std::cout << "Login successful!\n";
+			if (login(currentUser)) { //call login
+				std::cout << "Login successful!\n";
 
-	            if (currentUser.role == 1) { //for admin role
-		            std::cout << "Redirecting to admin menu...\n";
-		            showAdminMenu(currentUser);
-	            }
-	            else if (currentUser.role == 0) { //for student role
-		            std::cout << "Redirecting to user menu...\n";
-		            loadUserCourses(currentUser); //load user profile from storage.cpp
-		            showUserMenu(currentUser);
-	            }
-            }	
+				if (currentUser.role == 1) { //for admin role
+					std::cout << "Redirecting to admin menu...\n";
+					showAdminMenu(currentUser);
+				}
+				else if (currentUser.role == 0) { //for student role
+					std::cout << "Redirecting to user menu...\n";
+					loadUserCourses(currentUser); //load user profile from storage.cpp
+					showUserMenu(currentUser);
+				}
+			}
 			else {
 				std::cout << "Login failed! Please try again.\n";
 			}
