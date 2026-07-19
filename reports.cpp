8dc43dbcd7c740bc1004132f71e387ef774243a5
Kai Jun <ngkj-wm26@student.tarc.edu.my>
2026-07-20 00:24:50 +0800

    report file not yet finish.

---
+++ reports.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include "models.h"
+#include "menu.h"
+
+void showReportModule(const std::vector<User>& studentList) {
+
+	std::cout << "Report\n\n";
+}
+
+
