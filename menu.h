6528a6895dea8ababfa2f90f2509c678a27c3662
Kai Jun <ngkj-wm26@student.tarc.edu.my>
2026-07-20 00:22:28 +0800

    remove void report

--- menu.h
+++ menu.h
@@ -1,9 +1,7 @@
 #ifndef MENU
 #define MENU
-#include <vector>
 
 void showUserMenu(User& currentUser); // User is struct , & is pass by reference
 void showAdminMenu(User& currentUser);
-void showReportModule(const std::vector<User>& studentList);
 
 #endif
