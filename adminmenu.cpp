23b2eff670fb95de073064915a0ad3c3e06f8193
Mvict-66 <pianomusic0606@gmail.com>
2026-07-21 13:38:58 +0800

    Update adminmenu.cpp
    added "" b4 \n at line 26

---
+++ adminmenu.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include "models.h"
+#include "menu.h"
+#include "report.h"
+#include "tools.h"
+#include "adminUserFunctions.h"
+
+
+//Only can be access by admin
+void showAdminMenu(User& currentUser) {
+	std::vector<User> studentList;
+	//loop
+	int subchoice;
+	bool loggedin = true;
+
+	//Output that will be shown on screen after entering into the admin account
+	while (loggedin) {
+		std::cout << "==========\n";
+		std::cout << "Admin Menu\n";
+		std::cout << "==========\n";
+		std::cout << "Welcome, Admin.\n\n";
+
+		std::cout << "Choose one option by typing number:\n1. User Module\n2. Subject Module\n3. Schedule Module\n4. Booking Module\n5. Reporting Module\n6. Back to Main Menu\n";
+		std::cout << std::setfill('=') << std::setw(50) << "" << '\n';
+
+		subchoice = intgerinputfilter("Enter your choice(1-6)= ");
+
+
+		//using switch instead of if else
+		switch (subchoice) {
+		case 1:
+			userModulePortal();
+			break;
+		case 2:
+			//subject module
+		case 3:
+			//schedule module
+			break;
+		case 4:
+			//booking module
+			break;
+		case 5:
+			std::cout << "\n[!] Opening Reporting Module dashboard......\n";//load the report module
+			showReportModule(studentList);
+			break;
+		case 6:
+			loggedin = false;
+			std::cout << "Logging out......\n\n";
+			break;
+			//Using default to avoid user type in the wrong input
+		default:
+			std::cout << "Invalid input! Please enter a valid number.\n\n";
+			break;
+		}
+
+	}
+}
