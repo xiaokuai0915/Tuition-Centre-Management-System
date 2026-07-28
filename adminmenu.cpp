88129d2fd9360278b1b95e35eee7a8a573d65b0b
Kai Jun <ngkj-wm26@student.tarc.edu.my>
2026-07-28 21:50:36 +0800

    Add on output message for each module
    and remove (const std::vector<User>&studentList) at showReportModule

---
+++ adminmenu.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include "../Utils/models.h"
+#include "../Utils/menu.h"
+#include "report.h"
+#include "../Utils/tools.h"
+#include "adminUserFunctions.h"
+
+
+//Only can be access by admin
+void showAdminMenu(User& currentUser) {
+	//loop
+	int subchoice;
+	bool loggedin = true;
+
+	//Output that will be shown on screen after entering into the admin account
+	while (loggedin) {
+		std::cout << "\n==========\n";
+		std::cout << "Admin Menu\n";
+		std::cout << "==========\n";
+		std::cout << "Welcome, Admin.\n\n";
+
+		std::cout << "Choose one option by typing number:\n1. User Module\n2. Subject Module\n3. Schedule Module\n4. Booking Module\n5. Reporting Module\n0. Back to Main Menu\n";
+		std::cout << std::setfill('=') << std::setw(50) << '\n';
+
+		subchoice = intgerinputfilter("\nEnter your choice(0-5)= ");
+		if (subchoice == -1) {
+			std::cout << "Invalid input! Please enter a valid number\n";
+			continue;
+		}
+		if (subchoice == -2) {
+			std::cout << "Input cannot be empty! Please enter a valid number\n";
+			continue;
+		}
+
+
+		//using switch instead of if else
+		switch (subchoice) {
+		case 1:
+			std::cout << "\n[!] Opening User Module dashboard......\n";//load the user module
+			userModulePortal(currentUser);
+			break;
+		case 2:
+			std::cout << "\n[!] Opening Subject Module dashboard......\n";//load the subject module
+			//subject module
+			break;
+		case 3:
+			std::cout << "\n[!] Opening Schedule Module dashboard......\n";//load the schedule module
+			//schedule module
+			break;
+		case 4:
+			std::cout << "\n[!] Opening Booking Module dashboard......\n";//load the booking module
+			//booking module
+			break;
+		case 5:
+			std::cout << "\n[!] Opening Reporting Module dashboard......\n";//load the report module
+			showReportModule();
+			break;
+		case 0:
+			loggedin = false;
+			std::cout << "Logging out......\n\n";
+			break;
+		default:
+			std::cout << "Invalid input! Please enter a valid number.\n\n";
+			break;
+		}
+
+	}
+}
